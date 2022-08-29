49b6398eb5a1d79e109727d6cd510459355679ea
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2022-08-29 18:23:43 +0300

    Initial Commit

---
+++ ToDOEr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDOEr
+//
+//  Created by Oleksandr Smakhtin on 29.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
